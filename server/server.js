b8e1c71624c68f7120ac454995459aa20665d31f
Ross Jourdain <ross.jourdain@gmail.com>
2019-04-30 09:42:51 +1200

    Setup basic backend

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const server = express()
+
+// JSON API middleware
+server.use(express.json())
+
+// Static file serving middleware
+server.use(express.static('./public'))
+
+
+
+module.exports = server
