ca9bd731909e6f21ac42127921ca69b012404df3
Raichung <cliff.ahleong@gmail.com>
2019-05-10 15:53:40 +1200

    clear repo

--- server/db/emoji.js
+++
@@ -1,9 +0,0 @@
-const connection = require('./connection')
-
-function getEmoji (db = connection) {
-  return db('emoji').select()
-}
-
-module.exports = {
-  getEmoji
-}
--- server/server.js
+++ server/server.js
@@ -1,6 +1,4 @@
 const express = require('express')
-const emojiRoutes = require('./routes/emoji')
-const authRoutes = require('./routes/auth')
 
 const server = express()
 
@@ -10,10 +8,5 @@ server.use(express.json())
 // Static file serving middleware
 server.use(express.static('./public'))
 
-// Add emoji routes, starting at /api/emoji
-server.use('/api/emoji', emojiRoutes)
-
-// POST /api/v1/auth/register
-server.use('/api/v1/auth', authRoutes)
 
 module.exports = server
