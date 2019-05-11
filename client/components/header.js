d6c634409b47e59031e63cfac4c54b9a1133aac6
Raichung <cliff.ahleong@gmail.com>
2019-05-11 18:18:52 +1200

    navbar semi done

---
+++ client/components/header.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import MenuIcon from '@material-ui/icons/Menu';
+import IconButton from '@material-ui/core/IconButton';
+
+
+
+
+
+
+class header extends Component {
+
+
+ 
+
+
+  render() {
+    return (
+      <AppBar
+      position="fixed"
+      style={{
+        backgroundColor:  '#2f2f2f' , 
+          boxShadow: 'none',
+          padding: '10px 0px'
+      }}
+  >
+  <Toolbar>
+
+<div className="header_logo">
+    <div className="font_righteous header_logo_daycare">Daycare2.0</div>
+    <div className="header_logo_title">The Waitlist</div>
+</div>
+
+<IconButton
+                        aria-label="Menu"
+                        color="inherit"
+                        onClick={()=> console.log('clicked')}
+                    >
+                        <MenuIcon/>
+
+                    </IconButton> 
+
+
+</Toolbar>
+
+
+
+  </AppBar>
+    );
+  }
+}
+
+export default header;
