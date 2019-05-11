ff4f95ea600fff9b31f6d2e7fe9e63f2a42c6cce
Raichung <cliff.ahleong@gmail.com>
2019-05-11 16:27:26 +1200

    starting

--- client/index.js
+++ client/index.js
@@ -4,7 +4,7 @@ import { compose, createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
 
-import App from './components/App'
+import App from './app'
 import reducers from './reducers'
 
 // No Thunk
