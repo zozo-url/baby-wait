0def97cdd3a3df45843e0f440c0374c7a1cf6c64
Raichung <cliff.ahleong@gmail.com>
2019-05-11 16:19:26 +1200

    latest

---
+++ client/components/ChildProfile.jsx
@@ -0,0 +1,27 @@
+import React from 'React'
+
+
+
+class ChildProfile extends React.Component{
+  constructor(props){
+    super(props)
+    this.state={}
+  }
+  render(){
+    return(
+      <div>
+
+      </div>
+    )
+  }
+} 
+
+
+
+
+
+
+
+
+
+export default ChildProfile
