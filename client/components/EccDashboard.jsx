4a58d1bcb1c4ebf97ed2ffc6e45a00df85263220
SharleneRyan <sa.ryan@live.com>
2019-05-14 17:11:27 +1200

    adding pending list for eccDashboard

--- client/components/EccDashboard.jsx
+++ client/components/EccDashboard.jsx
@@ -2,35 +2,41 @@ import React from 'react'
 import { HashRouter as Router, Route, Link }  from 'react-router-dom'
 import { connect } from 'react-redux'
 import * as actions from '../actions'
+import { getEccPendingData } from "../apis/api"
 
 class EccDashboard  extends React.Component{
   constructor() {
     super()
     this.state = { 
       currentUser: 1,
-      value: ''
+      value: []
     }
     // this.getEccPendingData(this.getEccPendingData).bind(this)
   }
   componentWillMount() {
     // this.getEccPendingData(this.state.currentUser)
-    this.setState({value: this.props.EccWaitlistInfo()})
+       getEccPendingData(1, (err,data) => {
+           this.setState({value: data});
+          
+       });
   }
 
 
   render () {
+     console.log(this.state.value)
   return (
     <div className='Dash'> 
       <h1></h1>
       <br/>
       <br/>
       <div className="header"></div>
-      <h1 className='DashHeader'>Your Waitlist</h1>
+      <h1 className='DashHeader'>Your Pending Waitlist</h1>
       <div className="main-container">
-      {this.state.value.payload.map((item, id) => {
+      {/* {this.state.value.length > 0 ? this.state.value[0].first_name : ""} */}
+      {this.state.value.map((item,id) => {
         return <div>
-          <p key={id} className='DashText'>{item.child_rank}. {item.child_first_name} {item.child_last_name}  <button>x</button></p> 
-            <p className='DashSubText'>{item.parent_first_name} {item.parent_last_name}    {item.parent_email}</p>
+          <p key={id} className='DashText'>{item.child_rank} {item.first_name} {item.last_name}  <button>x</button></p> 
+            {/* <p className='DashSubText'>{item.parent_first_name} {item.parent_last_name}    {item.parent_email}</p> */}
           </div>
       })}
       <br/>
@@ -43,7 +49,7 @@ class EccDashboard  extends React.Component{
 }
 const mapStateToProps = (state) => {
   return {
-    data : state.eccWaitlist
+    data : state.ecc
   }
 }
 
