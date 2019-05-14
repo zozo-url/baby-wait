60c6d9d099f2eff6c90ef1eecf251bf6aead0dc7
Devon Lumsden <devonlumsden@mail.com>
2019-05-14 19:34:16 +1200

    child list change for users children

--- seeds/child.js
+++ seeds/child.js
@@ -5,9 +5,10 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('child').insert([
-        {id: 1, first_name: 'John1', last_name: 'smith', date_of_birth: '10/05/2018', parent_id: 3 },
+        {id: 1, first_name: 'John1', last_name: 'smith', date_of_birth: '10/05/2018', parent_id: 3},
         {id: 2, first_name: 'Abi2',last_name: 'lodo', date_of_birth: '2/3/2019', parent_id: 1 },
-        {id: 3, first_name: 'sarah3',last_name: 'wiseman', date_of_birth: '1/3/2019', parent_id: 2 }
+        {id: 3, first_name: 'sarah3',last_name: 'wiseman', date_of_birth: '1/3/2019', parent_id: 2 },
+        {id: 4, first_name: 'logan',last_name: 'wiseman', date_of_birth: '1/3/2019', parent_id: 2 }
       ]);
     });
 };
